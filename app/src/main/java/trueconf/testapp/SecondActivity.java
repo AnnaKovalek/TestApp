bf76eda358c12d06f3f9288498a3f5ac4ce494f8
AnnaKovalek <anna.kovalek@gmail.com>
2015-12-24 19:45:19 +0200

    Initial commit

---
+++ app/src/main/java/trueconf/testapp/SecondActivity.java
@@ -0,0 +1,16 @@
+package trueconf.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anna on 24.12.15.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
